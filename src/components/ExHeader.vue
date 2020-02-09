0873b34960fbd98813c06129fea338be4f104590
Anderson Contreras <alexandearko@gmail.com>
2020-02-09 16:14:40 -0400

    Mejorar proyecto: links y conversión

--- src/components/ExHeader.vue
+++ src/components/ExHeader.vue
@@ -9,7 +9,14 @@
         >ExchangeSystem</router-link>
       </div>
       <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow"></div>
+        <div class="text-sm lg:flex-grow">
+          <router-link
+            v-for="l in links"
+            :key="l.title"
+            :to="l.to"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          >{{ l.title }}</router-link>
+        </div>
       </div>
     </nav>
   </header>
@@ -20,6 +27,13 @@ import ExIcon from "@/components/ExIcon";
 
 export default {
   name: "ExHeader",
-  components: { ExIcon }
+  components: { ExIcon },
+
+  props: {
+    links: {
+      type: Array,
+      default: () => {}
+    }
+  }
 };
 </script>
